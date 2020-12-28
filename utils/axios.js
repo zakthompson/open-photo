1966f6582c1c46057756712b6cacaa1a950e5dd7
zakthompson <zak@thisfireinside.com>
2020-12-27 19:11:50 -0500

    Axios interceptor to redirect 401s to login page

--- utils/axios.js
+++ utils/axios.js
@@ -7,4 +7,16 @@ const instance = axios.create({
   },
 });
 
+instance.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  (e) => {
+    if (window && e.response.status === 401) {
+      window.location.href = '/api/auth/login';
+    }
+    return Promise.reject(e);
+  },
+);
+
 export default instance;
